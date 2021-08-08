d0d3fbea5afc7624a6dbaae6793e740c4ad1924a
SRC0747 <srcsrk1998@gmail.com>
2021-08-08 20:37:53 +0530

    Calculating quotient and remainder.

---
+++ src/QuotientRemainder.java
@@ -0,0 +1,11 @@
+public class QuotientRemainder {
+    public static void main(String[] args)
+    {
+        int dividend=22;
+        int divisor=3;
+        int quotient=(dividend/divisor);
+        int remainder=(dividend%divisor);
+        System.out.println("Quotient is:"+quotient);
+        System.out.println("Remainder is:"+remainder);
+    }
+}
